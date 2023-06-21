41ab9c477fa81720627dd6d8f84e49a3c41ee730
Grym68 <oliver.matterpinster@gmail.com>
2023-06-21 16:25:03 +0100

    Introduced in JUnitTests, and Integration Tests and @Before and @After annotations, also fixed the building of Gradle properly and CLI Gradle

---
+++ UnitTestingPractice/app/src/androidTest/java/co/uk/practice/unittestingpractice/ResouseComparerTest.kt
@@ -0,0 +1,39 @@
+package co.uk.practice.unittestingpractice
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ResouseComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @After
+    fun teardown() {
+        resourceComparer = null
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "UnitTestingPractice")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Hello")
+        assertThat(result).isFalse()
+    }
+
+
+}
